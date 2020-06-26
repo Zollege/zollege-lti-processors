9082bfa2b71dd578c38f216febbb682ab71aba5c
Alejandro Cardenas <alejandro.cardenas@edunext.co>
2020-06-26 07:38:44 -0500

    Adding course info provider and adding user_id and ExternalUserID to personal_user_info params

--- zollege_lti/processors.py
+++ zollege_lti/processors.py
@@ -2,7 +2,7 @@
 Common LTI processors for Zollege.
 """
 from helpers import get_user
-
+from six import text_type
 
 def personal_user_info(xblock):
     """
@@ -16,7 +16,10 @@ def personal_user_info(xblock):
     names_list = user_full_name.split(' ', 1)
 
     params = {
+        'lis_person_contact_email_primary': user.email,
         'lis_person_name_given': names_list[0],
+        'user_id': text_type(user.id),
+        'ExternalUserID': user.username,
     }
 
     if len(names_list) > 1:
@@ -26,6 +29,24 @@ def personal_user_info(xblock):
 
 
 personal_user_info.lti_xblock_default_params = {
+    'lis_person_contact_email_primary': '',
     'lis_person_name_given': '',
     'lis_person_name_family': '',
+    'user_id': '',
+    'ExternalUserID': '',
+}
+
+def course_info(xblock):
+    """
+    Provide additional course information.
+    """
+    params = {
+        'course_id': text_type(xblock.parent.course_key),
+        'location': xblock.parent.course_key.org,
+    }
+    return params
+
+course_info.lti_xblock_default_params = {
+    'course_id': '',
+    'location': '',
 }
